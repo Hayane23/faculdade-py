32a1db6ce2a55a9471a9b09668ec0c344c0a40d5
Hayane Silva <hayane.silva23@gmail.com>
2020-09-01 16:49:49 -0300

    Corrigir estrutura de repetição.

--- exercicio4.py
+++ exercicio4.py
@@ -4,16 +4,18 @@ peso = float(input('Qual o seu peso atualmente? '))
 altura = float(input('E qual a sua altura? '))
 imc = peso / (altura * altura)
 if imc < 17:
+    print('debug 1')
     print('Seu IC atual é de: ', imc, 'Você está muito abaixo do peso.')
-elif imc >= 17 < 18.50:
+elif imc >= 17 and imc < 18.50:
+    print('debug 2')
     print('Seu IMC atual é de: ', imc, 'Você está abaixo do peso.')
-elif imc >= 18.50 < 24.99:
+elif imc >= 18.50 and imc < 24.99:
     print('Seu IMC atual é de: ', imc, 'Você está em seu peso normal.')
-elif imc >= 24.99 < 30:
+elif imc >= 24.99 and imc < 30:
     print('Seu IMC atual é de: ', imc, 'Você está acima do peso.')
-elif imc >= 30 < 35:
+elif imc >= 30 and imc < 35:
     print('Seu IMC atual é de: ', imc, 'Você está com obesidade I.')
-elif imc >= 35 < 40:
+elif imc >= 35 and imc < 40:
     print('Seu IMC atual é de: ', imc, 'Você está com obesidade II.')
 else:
     print('Seu IMC é: ', imc, 'Você está com obesidade III.' )
