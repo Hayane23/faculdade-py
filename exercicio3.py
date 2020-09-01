7280d6137ddee6f1905744164c6fd2204805700b
Hayane Silva <hayane.silva23@gmail.com>
2020-09-01 11:50:05 -0300

    Alterar paremetros de variavel.

--- exercicio3.py
+++ exercicio3.py
@@ -1,9 +1,10 @@
 print('======== exercicio 03 =======')
 print('Calculo de velocidade.')
-sZero = int(input('Qual a posição inicial em metros: '))
-vZero = int(input('Qual a velocidade inicial em metros por segundo: '))
+s_Zero = float(input('Qual a posição inicial em metros: '))
+v_Zero = float(input('Qual a velocidade inicial em metros por segundo: '))
 tempo = int(input('Qual o tempo de movimento: '))
-equacaoS = (sZero + vZero) * tempo
-print ('A posição final é: ', equacaoS)
+equacao_S = (s_Zero + v_Zero) * tempo
+print('A posição final é: ', equacao_S)
 
 #Deus tenha mizericordia, por que eu nunca aprendi calculo em fisica?
+#Quem tem amigo tem tudo!
