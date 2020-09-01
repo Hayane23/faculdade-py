262f76de7b3ff153105f849a2e6b8a53eb24e5b1
Hayane Silva <hayane.silva23@gmail.com>
2020-09-01 17:25:00 -0300

    Remover debug

--- exercicio4.py
+++ exercicio4.py
@@ -4,10 +4,8 @@ peso = float(input('Qual o seu peso atualmente? '))
 altura = float(input('E qual a sua altura? '))
 imc = peso / (altura * altura)
 if imc < 17:
-    print('debug 1')
     print('Seu IC atual é de: ', imc, 'Você está muito abaixo do peso.')
 elif imc >= 17 and imc < 18.50:
-    print('debug 2')
     print('Seu IMC atual é de: ', imc, 'Você está abaixo do peso.')
 elif imc >= 18.50 and imc < 24.99:
     print('Seu IMC atual é de: ', imc, 'Você está em seu peso normal.')
