e51f1d2dc797a9471571639c5f7846dc459d1045
Hayane Silva <hayane.silva23@gmail.com>
2020-09-01 11:52:49 -0300

    execicio 5

---
+++ exercicio5.py
@@ -0,0 +1,7 @@
+print('======== exercicio 05 =======')
+print('Vamos calcular a media de três números.')
+primeiro_Numero = int(input('Insira o primeiro número: '))
+segundo_Numero = int(input('Insira o segundo número: '))
+terceiro_Numero = int(input('Insira o terceiro número: '))
+media = (primeiro_Numero + segundo_Numero + terceiro_Numero)/3
+print('A média da some é: ', media)
