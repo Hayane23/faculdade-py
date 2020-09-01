a2fd51aa3cf4b43be0fa9df38f5d96ec23c0c4a5
Hayane Silva <hayane.silva23@gmail.com>
2020-09-01 10:48:03 -0300

    Exercicio 3.

---
+++ exercicio3.py
@@ -0,0 +1,9 @@
+print('======== exercicio 03 =======')
+print('Calculo de velocidade.')
+sZero = int(input('Qual a posição inicial em metros: '))
+vZero = int(input('Qual a velocidade inicial em metros por segundo: '))
+tempo = int(input('Qual o tempo de movimento: '))
+equacaoS = sZero + vZero * tempo
+print ('A posição final é: ', equacaoS)
+
+#Deus tenha mizericordia, por que eu nunca aprendi calculo em fisica?
