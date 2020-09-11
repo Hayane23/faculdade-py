4beace9accc406a3349edc9036362220385474d6
Hayane Silva <hayane.silva23@gmail.com>
2020-09-11 10:34:57 -0300

    Aula de labortirio 3 ex.5.

---
+++ lab3-ex5.py
@@ -0,0 +1,8 @@
+print('======== exercicio 05 =======')
+p_numero = int(input('Insira o primeiro número: '))
+s_numero = int(input('Insira o segundo número: '))
+
+if p_numero > s_numero:
+    print(p_numero, 'é maior que', s_numero)
+else:
+    print(s_numero, 'é maior que', p_numero)
